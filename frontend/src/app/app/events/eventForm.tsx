3c7f18ac7cd0fcdb466dc43e4dc58c22d7500cca
Mathiconroy <mathiconroy@gmail.com>
2023-07-30 19:02:29 -0400

    Add eventForm.tsx

---
+++ frontend/src/app/app/events/eventForm.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { FormEvent } from "react";
+
+export default function EventForm() {
+  return (
+    <form id="eventForm" onSubmit={(e) => handleEventForm(e)}>
+      <EventFormInput type="text" id="name" name="Name" isTextarea={false}/>
+      <EventFormInput type="text" id="date" name="Date" isTextarea={false}/>
+      <EventFormInput id="description" name="Description" isTextarea={true}/>
+      <button className="my-2 rounded-lg bg-blue-500 text-white p-4">Create event</button>
+    </form>
+  );
+}
+
+function EventFormInput({
+  name,
+  id,
+  type,
+  isTextarea,
+}: {
+  name: string,
+  id: string,
+  type?: string,
+  isTextarea: boolean,
+}) {
+  return (
+    <div>
+      <label htmlFor={id} className="block py-2">{name}</label>
+      {isTextarea ? <textarea name={name} id={id} className="border w-full block rounded-lg p-2"/> : 
+        <input type={type} name={name} id={id} className="border w-full block rounded-lg p-2"/>}
+    </div>
+  );
+}
+
+function handleEventForm(e: FormEvent) {
+  e.preventDefault();
+}
--- frontend/src/app/app/events/page.tsx
+++ frontend/src/app/app/events/page.tsx
@@ -1,7 +1,10 @@
+import EventForm from "./eventForm";
+
 export default function EventList() {
   return (
-    <div className="m-4 rounded-lg border bg-white drop-shadow-md">
-      <h2 className="p-5">Events</h2>
+    <div className="m-4 px-5 py-8 rounded-lg border bg-white drop-shadow-md">
+      <h1 className="p-2">Events</h1>
+      <EventForm />
     </div>
   );
 }
