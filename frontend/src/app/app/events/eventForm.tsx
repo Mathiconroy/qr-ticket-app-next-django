ac7c1ff8425dbc7a9ee26bbf8d84a14b599e00fa
Mathiconroy <mathiconroy@gmail.com>
2023-08-26 19:37:08 -0400

    fixed input types in eventForm

--- frontend/src/app/app/events/eventForm.tsx
+++ frontend/src/app/app/events/eventForm.tsx
@@ -6,9 +6,9 @@ import axiosInstance from "@/app/axiosInstance";
 export default function EventForm() {
   return (
     <form id="eventForm" onSubmit={(e) => handleEventForm(e)}>
-      <EventFormInput type="text" id="name" name="Name" isTextarea={false}/>
-      <EventFormInput type="text" id="date" name="Date" isTextarea={false}/>
-      <EventFormInput id="description" name="Description" isTextarea={true}/>
+      <EventFormInput type="text" id="name" label="Name" name="name" isTextarea={false}/>
+      <EventFormInput type="datetime-local" label="Date" id="scheduled_datetime" name="scheduled_datetime" isTextarea={false}/>
+      <EventFormInput id="description" label="Description" name="description" isTextarea={true}/>
       <button className="mt-3 rounded-lg bg-blue-500 text-white p-3">Create event</button>
     </form>
   );
@@ -18,16 +18,18 @@ function EventFormInput({
   name,
   id,
   type,
+  label,
   isTextarea,
 }: {
   name: string,
   id: string,
   type?: string,
+  label: string,
   isTextarea: boolean,
 }) {
   return (
     <div>
-      <label htmlFor={id} className="block py-2">{name}</label>
+      <label htmlFor={id} className="block py-2">{label}</label>
       {isTextarea ? <textarea name={name} id={id} className="border w-full block rounded-lg p-2"/> : 
         <input type={type} name={name} id={id} className="border w-full block rounded-lg p-2"/>}
     </div>
@@ -36,11 +38,8 @@ function EventFormInput({
 
 function handleEventForm(e: FormEvent) {
   e.preventDefault();
-  axiosInstance.post('events/', {
-    created_by: 1,
-    scheduled_datetime: "2023-08-19T23:23:18Z",
-    description:"My first event :D",
-  }, {
-
-  });
+  const form = document.querySelector("#eventForm") as HTMLFormElement;
+  const formData = new FormData(form);
+  formData.append("created_by", "1");
+  axiosInstance.post('events/', formData);
 }
