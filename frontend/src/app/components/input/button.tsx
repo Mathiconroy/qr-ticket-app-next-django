085696b5ef2b966deac886df28653302becbb646
Mathiconroy <mathiconroy@gmail.com>
2023-09-03 19:54:40 -0400

    Added generic reusable button component

---
+++ frontend/src/app/components/input/button.tsx
@@ -0,0 +1,11 @@
+export default function InputButton({
+  text,
+  className = "",
+}: {
+  text: string,
+  className?: string,
+}) {
+  return (
+    <button className={`rounded-lg bg-blue-500 text-white p-3 ${className}`}>{text}</button>
+  )
+}
