e555910555240af1983e595ce7df010136046904
Mathiconroy <mathiconroy@gmail.com>
2023-09-09 11:59:50 -0400

    Add custom ObtainAuthToken view

--- backend/qr_tickets/urls.py
+++ backend/qr_tickets/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import LoginUser, WhoAmI, EventList
+from .views import LoginUser, WhoAmI, EventList, CustomAuthToken
 from rest_framework.authtoken import views
 
 urlpatterns = [
-    path("api-token-auth/", views.obtain_auth_token),
+    path("api-token-auth/", CustomAuthToken.as_view()),
     path("login/", LoginUser.as_view()),
     path("whoami/", WhoAmI.as_view()),
     path("events/", EventList.as_view()),
--- backend/qr_tickets/views.py
+++ backend/qr_tickets/views.py
@@ -1,5 +1,8 @@
-from rest_framework.views import APIView
+
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from django.contrib.auth import authenticate, login
 from rest_framework import mixins
 from rest_framework import generics
@@ -47,3 +50,22 @@ class EventList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.Generic
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
+
+
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data,
+            context={'request': request}
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        response = Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
+        response.set_cookie('token', token.key)
+        return response
