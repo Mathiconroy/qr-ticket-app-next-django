1b16b9038612f9f60f04def2c4be49455bfae5dc
Mathiconroy <mathiconroy@gmail.com>
2023-11-09 00:42:19 -0300

    Add event card rendering

--- frontend/src/app/app/events/eventDashboard.tsx
+++ frontend/src/app/app/events/eventDashboard.tsx
@@ -1,23 +1,50 @@
+"use client";
+
 import InputButton from "@/app/components/input/button";
+import axiosInstance from "@/app/axiosInstance";
+import { Key } from "react";
+import { useEffect, useState } from "react";
+
+interface Event {
+  id: Key,
+  created_by: Number,
+  name: string,
+  scheduled_datetime: string,
+  description: string,
+  created_at: string,
+  edited_at: string,
+}
 
 export default function EventDashboard() {
+  const [eventData, setEventData] = useState<Event[]>();
+  useEffect(() => {
+    async function fetchEventData() {
+      const eventResponse = await axiosInstance.get('events/');
+      setEventData(eventResponse.data);
+    }
+    fetchEventData();
+  }, []);
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      <EventCard />
+      {eventData !== undefined ? eventData.map((event: Event) => <EventCard key={event.id} event={event} />) : ''}
     </div>
   );
 }
 
-function EventCard() {
+function EventCard({
+  event 
+}: {
+  event: Event 
+}) {
   return (
     <div className="border-2 rounded-lg">
-      <div className="p-2 font-bold text-lg">Event Name</div>
+      <div className="p-2 font-bold text-lg">{event.name}</div>
       <div className="">Time</div>
-      <div className="">Description</div>
+      <div className="">Description: {event.description}</div>
       <div className="">Tickets sold</div>
       <div className="">Tickets left</div>
       <InputButton className="m-2" text="Edit"/>
       <InputButton text="Details"/>
     </div>
-  )
+  );
 }
