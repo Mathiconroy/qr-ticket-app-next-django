83f8ff9fb9a92e2300ea6d297c06416c3678839f
Mathiconroy <mathiconroy@gmail.com>
2024-02-04 14:36:29 -0300

    Change str representation of models

--- backend/qr_tickets/models.py
+++ backend/qr_tickets/models.py
@@ -25,7 +25,7 @@ class Event(models.Model):
     edited_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return f'{self.id} - {self.name}'
 
 
 class TicketType(models.Model):
@@ -34,7 +34,7 @@ class TicketType(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return self.name
+        return f'{self.id} - {self.name}'
 
 
 class Ticket(models.Model):
