b1e9ab84c8b0616d1b12143797c0e90ab12f4798
Mathiconroy <mathiconroy@gmail.com>
2024-03-13 23:32:29 -0300

    Added RangeValidator

--- backend/qr_tickets/models.py
+++ backend/qr_tickets/models.py
@@ -32,6 +32,7 @@ class TicketType(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    max_quantity = models.IntegerField(null=True)
 
     def __str__(self):
         return f'{self.id} - {self.name}'
--- backend/qr_tickets/serializers.py
+++ backend/qr_tickets/serializers.py
@@ -1,6 +1,20 @@
+from decimal import Decimal
+
 from django.contrib.auth import get_user_model
 from qr_tickets.models import Event, TicketType, TicketOrderHeader, TicketOrderDetail
 from rest_framework import serializers
+import math
+
+
+class RangeValidator:
+    def __init__(self, minimum=math.inf, maximum=-math.inf):
+        self.minimum = minimum
+        self.maximum = maximum
+
+    def __call__(self, value):
+        if value < self.minimum or value > self.maximum:
+            message = f'Value {value} must be between {self.minimum} and {self.maximum}'
+            raise serializers.ValidationError(message)
 
 
 class UserSerializer(serializers.Serializer):
@@ -16,9 +30,11 @@ class EventSerializer(serializers.ModelSerializer):
 
 
 class TicketTypeSerializer(serializers.ModelSerializer):
+    price = serializers.DecimalField(max_digits=10, decimal_places=2, min_value=Decimal('0.00'))
+
     class Meta:
         model = TicketType
-        fields = ['id', 'event', 'name', 'price']
+        fields = ['id', 'event', 'name', 'price', 'max_quantity']
 
     def create(self, validated_data):
         TicketType.objects.create(
@@ -28,12 +44,18 @@ class TicketTypeSerializer(serializers.ModelSerializer):
         )
 
 
-# TODO: Look up ListField, might be useful.
+class TicketOrderDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TicketOrderDetail
+        fields = ['id', 'order_header', 'ticket_type', 'amount']
+
+
 class TicketOrderHeaderSerializer(serializers.ModelSerializer):
+    tickets = TicketOrderDetailSerializer(many=True)
+
     class Meta:
         model = TicketOrderHeader
         fields = ['id', 'buyer', 'event', 'created_at', 'tickets']
-        depth = 1
 
     def create(self, validated_data):
         ticket_order_header = TicketOrderHeader(
@@ -43,7 +65,6 @@ class TicketOrderHeaderSerializer(serializers.ModelSerializer):
         ticket_order_header.save()
 
         for ticket in validated_data['tickets']:
-            print(ticket)
             ticket_type = TicketType.objects.get(pk=ticket['ticket_type_id'])
             ticket_order_detail = TicketOrderDetail(
                 order_header=ticket_order_header,
@@ -51,9 +72,3 @@ class TicketOrderHeaderSerializer(serializers.ModelSerializer):
                 amount=ticket['amount']
             )
             ticket_order_detail.save()
-
-
-class TicketOrderDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TicketOrderDetail
-        fields = ['id', 'order_header', 'ticket_type', 'amount']
