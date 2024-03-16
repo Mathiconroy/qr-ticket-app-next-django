7d61f52a7f8fd59401a1be5afb390a85a265a031
Mathiconroy <mathiconroy@gmail.com>
2024-03-16 13:13:28 -0300

    Added title to page

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -10,6 +10,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>Ticketify</title>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
