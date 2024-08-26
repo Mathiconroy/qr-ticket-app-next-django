ee7673ae0382a6efdc42ba083458106fb955e4ab
Mathiconroy <mathiconroy@gmail.com>
2024-08-25 20:31:38 -0400

    Reworked login form

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,34 +1,42 @@
 "use client";
 
 import { FormEvent } from "react";
+import axiosInstance from "@/app/axiosInstance";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  async function handleLogin(e: FormEvent) {
+    e.preventDefault();
+    const form = document.querySelector("#loginForm") as HTMLFormElement;
+    const formData = new FormData(form);
+    const res = await axiosInstance.post("/api-token-auth", formData);
+    if (res.ok) {
+      router.push("/app");
+    }
+  }
   return (
-    <div className="grid h-screen grid-cols-1 place-items-center bg-neutral-900">
-      <div className="w-1/3 rounded-lg border border-zinc-800 bg-zinc-800 p-5">
-        <p className="text-xl">Log In</p>
+    <div className="grid h-screen grid-cols-1 place-items-center">
+      <div className="w-1/4 rounded-lg border bg-white border-gray-400 p-5">
+        <p className="text-center text-4xl font-bold mb-6">Login</p>
         <form id="loginForm" onSubmit={(e) => handleLogin(e)}>
-          <label htmlFor="username" className="block py-2">
-            Username
-          </label>
           <input
             type="text"
             id="username"
             name="username"
-            className="block w-full rounded-lg bg-zinc-700 p-2"
+            className="block w-full border-2 rounded-lg border-gray-200 mt-2 py-4 px-3"
+            placeholder="Username"
           />
-          <label htmlFor="password" className="block py-2">
-            Password
-          </label>
           <input
             type="password"
             id="password"
             name="password"
-            className="block w-full rounded-lg bg-zinc-700 p-2"
+            className="block w-full border-2 rounded-lg border-gray-200 mt-5 py-4 px-3"
+            placeholder="Password"
           />
           <button
             type="submit"
-            className="mt-5 w-full rounded-md bg-sky-700 p-2"
+            className="mt-5 w-full rounded-md bg-blue-400 p-2"
           >
             Log In
           </button>
@@ -37,7 +45,7 @@ export default function Home() {
         <button
           onClick={handleWhoAmI}
           type="submit"
-          className="mt-5 w-full rounded-md bg-sky-700 p-2"
+          className="mt-5 w-full rounded-md bg-blue-400 p-2"
         >
           Who am I?
         </button>
@@ -50,25 +58,9 @@ async function handleLogin(e: FormEvent) {
   e.preventDefault();
   const form = document.querySelector("#loginForm") as HTMLFormElement;
   const formData = new FormData(form);
-  const res = await fetch("http://127.0.0.1:8000/api/api-token-auth/", {
-    method: "POST",
-    body: formData,
-    credentials: "include",
-  });
-  if (res.ok) {
-    let json_response = await res.json();
-    localStorage.setItem("token", json_response["token"]);
-  } else {
-    // TODO: Handle this case lol.
-  }
+  const res = await axiosInstance.post("/api-token-auth/", formData);
 }
 
 async function handleWhoAmI() {
-  const res = await fetch("http://127.0.0.1:8000/api/whoami/", {
-    credentials: "include",
-    headers: {
-      Authorization: `Token ${localStorage.getItem("token")}`,
-    },
-  });
-  console.log(res.json());
+  const res = await axiosInstance.get("/whoami/");
 }
