4d5f30c42dd22b24cc12695528596bb51199e65a
Mathiconroy <mathiconroy@gmail.com>
2024-11-10 19:28:23 -0300

    Added shadcn to dependencies

--- frontend/src/app/app/events/[id]/page.tsx
+++ frontend/src/app/app/events/[id]/page.tsx
@@ -1,13 +1,14 @@
 'use client';
-import CardGrid from '@/app/components/display/cardGrid';
-import Card from '@/app/components/display/card';
-import TicketTypeForm from './ticketTypeForm';
-import axiosInstance from '@/app/axiosInstance';
+
+import CardGrid from '@/components/display/cardGrid';
+import Card from '@/components/display/card';
+import axiosInstance from '@/axiosInstance';
 import { useState, useEffect } from 'react';
-import FormButton from '@/app/components/input/button';
+import FormButton from '@/components/input/button/FormButton';
 import Link from 'next/link';
-import { TicketType } from '@/app/interfaces/interfaces';
-import Title from '@/app/components/display/title';
+import { TicketType } from '@/interfaces/interfaces';
+import Title from '@/components/display/title';
+import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from '@/components/ui/accordion';
 
 export default function EventDetails({ params }: { params: { id: number } }) {
   const [ticketTypeData, setTicketTypeData] = useState<TicketType[]>();
@@ -20,6 +21,20 @@ export default function EventDetails({ params }: { params: { id: number } }) {
   }, [params.id]);
   return (
     <>
+    <Accordion type={'single'} collapsible>
+      <AccordionItem value={'ticket-types'}>
+        <AccordionTrigger>Ticket Types</AccordionTrigger>
+        <AccordionContent>
+          INSERT TICKET TYPES TABLE HERE
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value={'tickets'}>
+        <AccordionTrigger>Tickets</AccordionTrigger>
+        <AccordionContent>
+          INSERT TICKET TABLE HERE
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
       <div className={'grid grid-cols-2 items-center gap-4'}>
         <Title>Ticket types</Title>
         <div className={'flex items-center justify-end gap-2'}>
