18133ee6676f84d8c432ebbf32f7e5dfd75a92d6
Mathiconroy <mathiconroy@gmail.com>
2024-11-22 22:26:51 -0300

    Add downloader

--- frontend/src/hooks/fetchHooks.ts
+++ frontend/src/hooks/fetchHooks.ts
@@ -1,23 +1,46 @@
 import useSWR from 'swr';
 import { TicketType } from '@/interfaces/interfaces';
 
-const fetcher = async (url: string): Promise<any> => {
+export const fetcher = async (url: string): Promise<any> => {
   const baseUrl = '/api';
   const fetchUrl = `${baseUrl}${url}`;
   const token = localStorage.getItem('token');
   const authorizationHeaderValue = token ? `Token ${token}` : '';
-  const res = await fetch(fetchUrl, {
+  const res = fetch(fetchUrl, {
     headers: {
       Authorization: authorizationHeaderValue
     }
   });
+  return res;
+};
+
+export const fetcherJson = async (url: string): Promise<any> => {
+  const res = await fetcher(url);
   return await res.json();
 };
 
+export const downloader = async (url: string): Promise<any> => {
+  const res = await fetcher(url);
+  const blob = await res.blob();
+  const blobUrl = window.URL.createObjectURL(new Blob([blob]));
+  const link = document.createElement('a');
+  link.href = blobUrl;
+  link.setAttribute('download', `FileName.pdf`);
+
+  // Append to html link element page
+  document.body.appendChild(link);
+
+  // Start download
+  link.click();
+
+  // Clean up and remove the link
+  link.remove();
+};
+
 export function useTicketTypes(id: number) {
-  return useSWR<TicketType[]>(`/events/${id}/ticketTypes/`, fetcher);
+  return useSWR<TicketType[]>(`/events/${id}/ticketTypes/`, fetcherJson);
 }
 
 export function useTicketOrders(id: number) {
-  return useSWR(`/events/${id}/ticketOrders/`, fetcher);
+  return useSWR(`/events/${id}/ticketOrders/`, fetcherJson);
 }
