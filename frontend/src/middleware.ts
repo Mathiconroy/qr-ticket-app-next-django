62cef142b75d5bf63c616f37c3ca3e57dd1a2e98
Mathiconroy <mathiconroy@gmail.com>
2024-12-22 16:42:53 -0300

    Stop using localhost in rewrites

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const rewriteUrl = new URL(`${request.nextUrl.pathname}/`, 'http://localhost:8000/');
+  const rewriteUrl = new URL(`${request.nextUrl.pathname}/`, 'http://127.0.0.1:8000/');
   return NextResponse.rewrite(rewriteUrl);
 }
 
