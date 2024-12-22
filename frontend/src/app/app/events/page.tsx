7ec9cce5ead0d24f78e4896c789400dcfdf5b9a7
Mathiconroy <mathiconroy@gmail.com>
2024-12-22 17:37:12 -0300

    Update button

--- frontend/src/app/app/events/page.tsx
+++ frontend/src/app/app/events/page.tsx
@@ -2,6 +2,7 @@ import EventListGrid from '@/components/events/EventListTable';
 import Title from '@/components/display/title';
 import Link from 'next/link';
 import FormButton from '@/components/input/button/FormButton';
+import { Button } from '@/components/ui/button';
 
 export default function EventList() {
   return (
@@ -9,7 +10,7 @@ export default function EventList() {
       <div className={'mb-2 flex items-center justify-between'}>
         <Title>My Events</Title>
         <Link href={'events/new/'}>
-          <FormButton text={'+ Create Event'} />
+          <Button>+ Create Event</Button>
         </Link>
       </div>
       <EventListGrid />
